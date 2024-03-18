652d9e7df30852c884d9be3034afd46c81baae26
Vanesa Bulnes <114104256+vanesabulnes@users.noreply.github.com>
2024-03-18 11:37:25 -0700

    Add files via upload

---
+++ BotMessage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BotMessage {
+    //automated response program 
+    /*
+    User message: "1", Reply: "Order confirmed"
+    User message: "2", Reply: "info@sololearn.com"
+    For any other number, the reply should be: "Try again". 
+    */
+    public static void main(String[] args) {
+        bot();
+    }
+    public static void bot(){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if( number == 1){
+            System.out.println("Order confirmed");
+        } 
+        else if(number == 2){
+            System.out.println("info@sololearn.com");
+        } else
+        System.out.println("Try again");
+    }
+}
